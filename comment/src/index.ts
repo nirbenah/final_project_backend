e0b48ad33aca04f572431ebc5b8925d5ac729cf2
adi-toledano <adi.toledano@campus.technion.ac.il>
2024-04-06 20:30:31 +0300

    fix

--- comment/src/index.ts
+++ comment/src/index.ts
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 import {
     getCommentsRoute,
@@ -22,7 +21,6 @@ export const API_GATEWAY_URL = process.env.API_GATEWAY_URL || "http://localhost:
 export const COMMENT_URL = process.env.COMMENT_URL || "http://localhost:5000";
 export const EVENT_URL = process.env.EVENT_URL || "http://localhost:6000";
 export const ORDER_URL = process.env.ORDER_URL || "http://localhost:7000";
-console.log('pass:', process.env.DBPASS);
 
 const dbURI = `mongodb+srv://admin:${process.env.DBPASS}@cluster0.vpn2j6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -39,7 +37,6 @@ const allowedOrigins = [API_GATEWAY_URL, COMMENT_URL, EVENT_URL, ORDER_URL];
 
 connectToDatabase();
 const app = express();
-app.use(bodyParser.json());
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
@@ -47,15 +44,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
-// const corsOptions = {
-//     origin: frontendURL,
-//     credentials: true,
-//   };
-// app.use(cors(corsOptions));
-
-app.post('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get(GET_COMMENTS_WITH_PAGINATION, getCommentsRoute);
 app.post(POST_COMMENT, postCommentRoute);
