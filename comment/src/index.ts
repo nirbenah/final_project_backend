97278408d607f44b83b4c7c6c8a78b9eea18735f
adi-toledano <adi.toledano@campus.technion.ac.il>
2024-04-06 20:06:44 +0300

    fix

--- comment/src/index.ts
+++ comment/src/index.ts
@@ -22,7 +22,7 @@ export const API_GATEWAY_URL = process.env.API_GATEWAY_URL || "http://localhost:
 export const COMMENT_URL = process.env.COMMENT_URL || "http://localhost:5000";
 export const EVENT_URL = process.env.EVENT_URL || "http://localhost:6000";
 export const ORDER_URL = process.env.ORDER_URL || "http://localhost:7000";
-
+console.log('pass:', process.env.DBPASS);
 
 const dbURI = `mongodb+srv://admin:${process.env.DBPASS}@cluster0.vpn2j6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
