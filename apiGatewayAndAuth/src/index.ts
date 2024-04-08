6a7538d01eee0b5f04e9f41ab78f302e52c40f81
adi-toledano <adi.toledano@campus.technion.ac.il>
2024-04-08 22:13:59 +0300

    commit

--- apiGatewayAndAuth/src/index.ts
+++ apiGatewayAndAuth/src/index.ts
@@ -27,7 +27,11 @@ async function connectToDatabase() {
         console.error('Error connecting to database:', error);
     }
 }
+<<<<<<< Updated upstream
 //console.log('pass:', process.env.DBPASS);
+=======
+
+>>>>>>> Stashed changes
 const port = process.env.PORT || 4000;
 const frontendURL = process.env.FRONTEND_URL || "http://localhost:5174";
 
